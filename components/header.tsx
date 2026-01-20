a615701d059fe6a95489ebbe4c4a0f1aed61f9f8
rovicz <roviczzf@gmail.com>
2026-01-20 20:07:42 -0300

    feat: adding cart icon at header

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export function Header() {
   return (
     <header className="h-[100px] w-full flex items-center px-[2.6875rem] border-b border-bottom-[1px] border-[#FFFFFF/21]">
-      <div className="container">
+      <div className="w-full flex items-center justify-between">
         <Link href="/">
           <Image
             src="/logo.svg"
@@ -14,6 +14,10 @@ export function Header() {
             priority
           />
         </Link>
+        <div className="flex items-center gap-[0.563rem] cursor-pointer">
+          <Image src="/bag.svg" alt="Carrinho" width={24} height={24} />
+          <span className="text-white font-medium">0</span>
+        </div>
       </div>
     </header>
   );
